a188d8a16501173fd7790fdb3a07737fe92ec04e
bataevvlad <bataevvlad@gmail.com>
2019-11-02 12:39:55 +0300

    start of project4

---
+++ Project4/Project4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project4
+//
+//  Created by Vlad Bataev on 11/2/19.
+//  Copyright © 2019 Vlad Bataev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
